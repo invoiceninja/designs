b1aa1b49d98aceb0b59ad92765c80708aa45bdb5
Benjamin Beganović <k1pstabug@gmail.com>
2020-09-07 10:58:26 +0200

    Add comment for compiler script

--- _compile.sh
+++ _compile.sh
@@ -1,3 +1,9 @@
+# This is compiler script that will iterate
+# over designs (folders) and compile them.
+
+# Required for this to work is sass compiler.
+# https://sass-lang.com/install
+
 for dir in `ls -1 .`; do
     if [[ -d $dir ]]; then
         sass "$dir/$dir.scss" "$dir/$dir.css";
